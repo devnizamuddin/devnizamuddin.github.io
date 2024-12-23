3399470c350efef80c930cfcbe6b62a162141074
devnizamuddin <dev.nizamuddin@gmail.com>
2024-12-23 23:49:17 +0600

    routing removed and home added

--- lib/dev_nizam_uddin.dart
+++ lib/dev_nizam_uddin.dart
@@ -1,18 +1,16 @@
+import 'package:devnizamuddin_github_io/app/modules/home/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
 
 class DevNizamUddin extends StatelessWidget {
   const DevNizamUddin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       title: "DevNizamUddin",
       debugShowCheckedModeBanner: false,
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+      home: HomeView(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:devnizamuddin_github_io/app/modules/home/controllers/home_controller.dart';
 import 'package:devnizamuddin_github_io/dev_nizam_uddin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:get/get.dart';
 
 void main() {
   usePathUrlStrategy();
+
+  Get.put(HomeController());
+
   runApp(const DevNizamUddin());
 }
