0d9f606b85a65bb7cd84a3872f37d93466a07fbc
devnizamuddin <dev.nizamuddin@gmail.com>
2024-12-24 04:24:45 +0600

    color file modified

--- lib/app/config/color.dart
+++ lib/app/config/color.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 // PRIMAPY
-const Color PRIMARY_COLOR = Color(0xff2C3E50);
+const Color PRIMARY_COLOR = Color(0xFF002147);
+const Color PRIMARY_COLOR_SECOND = Color(0xff34495E);
 const Color PRIMARY_LIGHT_COLOR = Color(0xff6C7A8C);
 const Color PRIMARY_DARK_COLOR = Color(0xff2E7D32);
 
 // SCAFFOLD
-const Color SCAFFOLD_BACKGROUND_COLOR = Color(0xffECF0F1);
+const Color SCAFFOLD_BACKGROUND_COLOR = PRIMARY_COLOR;
 const Color ACCENT_COLOR = Color(0xffE67E22);
 
 // INPUT FIELD
