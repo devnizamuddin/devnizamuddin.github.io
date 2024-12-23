42d62bf6372aa38be5db71b8e53bcdb09fa601e0
devnizamuddin <dev.nizamuddin@gmail.com>
2024-12-23 23:54:52 +0600

    made single module

---
+++ lib/app/home/controllers/home_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  String name = "Nizam Uddin Shamrat";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:devnizamuddin_github_io/app/modules/home/controllers/home_controller.dart';
+import 'package:devnizamuddin_github_io/app/home/controllers/home_controller.dart';
 import 'package:devnizamuddin_github_io/dev_nizam_uddin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
