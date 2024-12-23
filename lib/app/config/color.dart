83c94561ff914440f00975a42a75ae7d0641730d
devnizamuddin <dev.nizamuddin@gmail.com>
2024-12-24 01:44:45 +0600

    Color config added

---
+++ lib/app/config/color.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// PRIMAPY
+const Color PRIMARY_COLOR = Color(0xff2C3E50);
+const Color PRIMARY_LIGHT_COLOR = Color(0xff6C7A8C);
+const Color PRIMARY_DARK_COLOR = Color(0xff2E7D32);
+
+// SCAFFOLD
+const Color SCAFFOLD_BACKGROUND_COLOR = Color(0xffECF0F1);
+const Color ACCENT_COLOR = Color(0xffE67E22);
+
+// INPUT FIELD
+const Color ENABLED_BORDER_COLOR = Colors.grey;
+const Color FOCUSED_BORDER_COLOR = PRIMARY_COLOR;
+const Color ERROR_BORDER_COLOR = Color(0xfff72832);
+const Color FOCUSED_ERROR_BORDER_COLOR = Color(0xffb5020b);
+
+// TRANSPARENT
+const Color COLOR_TRANSPARENT = Color(0x00000000);
