b437dcd2003ac143f825debea8d58b8294d4bfd2
devnizamuddin <dev.nizamuddin@gmail.com>
2024-12-23 23:34:46 +0600

    flutter_web_plugins added to solve # in url

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(
     GetMaterialApp(
       title: "Application",
