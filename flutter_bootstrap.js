000bab7c9a417e1b46847264ac3ae5d1e9b2cddd
devnizamuddin <dev.nizamuddin@gmail.com>
2025-01-07 03:29:46 +0600

    version 1.0.4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "282886499"
+    serviceWorkerVersion: "2909169037"
   }
 });
